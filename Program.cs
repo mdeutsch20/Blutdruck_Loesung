5b086d06ca54452e12b06b8b9bc48597752e9c13
Manuel Deutsch <manuel.deutsch27@gmail.com>
2020-11-23 08:43:11 +0100

    git init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blutdruck_Loesung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
